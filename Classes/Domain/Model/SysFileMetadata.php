beb4187f23dc00e6f43b2acc2ce6226244298130
Michael Schams <typo3@2019.schams.net>
2019-01-02 14:22:35 +1100

    [TASK] Rework model "SysFileMetadata"

--- Classes/Domain/Model/SysFileMetadata.php
+++ Classes/Domain/Model/SysFileMetadata.php
@@ -14,25 +14,117 @@ namespace SchamsNet\AwsImageRecognition\Domain\Model;
  * @link        https://schams.net
  */
 
+use \TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
 /**
  * Model: SysFileMetadata
  */
-class SysFileMetadata extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
+class SysFileMetadata extends AbstractEntity
 {
     /**
      * Object 1
      *
+     * @access protected
      * @var string
      */
     protected $object1 = null;
 
     /**
+     * Object 2
+     *
+     * @access protected
+     * @var string
+     */
+    protected $object2 = null;
+
+    /**
+     * Object 3
+     *
+     * @access protected
+     * @var string
+     */
+    protected $object3 = null;
+
+
+    /**
+     * Object 4
+     *
+     * @access protected
+     * @var string
+     */
+    protected $object4 = null;
+
+    /**
+     * Object 5
+     *
+     * @access protected
+     * @var string
+     */
+    protected $object5 = null;
+
+    /**
+     * Object 6
+     *
+     * @access protected
+     * @var string
+     */
+    protected $object6 = null;
+
+    /**
+     * Object 7
+     *
+     * @access protected
+     * @var string
+     */
+    protected $object7 = null;
+
+    /**
+     * Object 8
+     *
+     * @access protected
+     * @var string
+     */
+    protected $object8 = null;
+
+    /**
+     * Object 9
+     *
+     * @access protected
+     * @var string
+     */
+    protected $object9 = null;
+
+    /**
+     * Object 10
+     *
+     * @access protected
+     * @var string
+     */
+    protected $object10 = null;
+
+    /**
+     * Object 11
+     *
+     * @access protected
+     * @var string
+     */
+    protected $object11 = null;
+
+    /**
+     * Object 12
+     *
+     * @access protected
+     * @var string
+     */
+    protected $object12 = null;
+
+    /**
      * Set object 1
      *
+     * @access public
      * @param string $object1
-     * @return void
      */
-    public function setObject1($object1)
+    public function setObject1($object1): void
     {
         $this->object1 = $object1;
     }
@@ -40,10 +132,253 @@ class SysFileMetadata extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
     /**
      * Return object 1
      *
+     * @access public
      * @return string $object1
      */
-    public function getObject1()
+    public function getObject1(): string
     {
         return $this->object1;
+    }
+
+    /**
+     * Set object 2
+     *
+     * @access public
+     * @param string $object2
+     */
+    public function setObject2($object2): void
+    {
+        $this->object2 = $object2;
+    }
+
+    /**
+     * Return object 2
+     *
+     * @access public
+     * @return string $object2
+     */
+    public function getObject2(): string
+    {
+        return $this->object2;
+    }
+
+    /**
+     * Set object 3
+     *
+     * @access public
+     * @param string $object3
+     */
+    public function setObject3($object3): void
+    {
+        $this->object3 = $object3;
+    }
+
+    /**
+     * Return object 3
+     *
+     * @access public
+     * @return string $object3
+     */
+    public function getObject3(): string
+    {
+        return $this->object3;
+    }
+
+    /**
+     * Set object 4
+     *
+     * @access public
+     * @param string $object4
+     */
+    public function setObject4($object4): void
+    {
+        $this->object4 = $object4;
+    }
+
+    /**
+     * Return object 4
+     *
+     * @access public
+     * @return string $object4
+     */
+    public function getObject4(): string
+    {
+        return $this->object4;
+    }
+
+    /**
+     * Set object 5
+     *
+     * @access public
+     * @param string $object5
+     */
+    public function setObject5($object5): void
+    {
+        $this->object5 = $object5;
+    }
+
+    /**
+     * Return object 5
+     *
+     * @access public
+     * @return string $object5
+     */
+    public function getObject5(): string
+    {
+        return $this->object5;
+    }
+
+    /**
+     * Set object 6
+     *
+     * @access public
+     * @param string $object6
+     */
+    public function setObject6($object6): void
+    {
+        $this->object6 = $object6;
+    }
+
+    /**
+     * Return object 6
+     *
+     * @access public
+     * @return string $object6
+     */
+    public function getObject6(): string
+    {
+        return $this->object6;
+    }
+
+    /**
+     * Set object 7
+     *
+     * @access public
+     * @param string $object7
+     */
+    public function setObject7($object7): void
+    {
+        $this->object7 = $object7;
+    }
+
+    /**
+     * Return object 7
+     *
+     * @access public
+     * @return string $object7
+     */
+    public function getObject7(): string
+    {
+        return $this->object7;
+    }
+
+    /**
+     * Set object 8
+     *
+     * @access public
+     * @param string $object8
+     */
+    public function setObject8($object8): void
+    {
+        $this->object8 = $object8;
+    }
+
+    /**
+     * Return object 8
+     *
+     * @access public
+     * @return string $object8
+     */
+    public function getObject8(): string
+    {
+        return $this->object8;
+    }
+
+    /**
+     * Set object 9
+     *
+     * @access public
+     * @param string $object9
+     */
+    public function setObject9($object9): void
+    {
+        $this->object9 = $object9;
+    }
+
+    /**
+     * Return object 9
+     *
+     * @access public
+     * @return string $object9
+     */
+    public function getObject9(): string
+    {
+        return $this->object9;
+    }
+
+    /**
+     * Set object 10
+     *
+     * @access public
+     * @param string $object10
+     */
+    public function setObject10($object10): void
+    {
+        $this->object10 = $object10;
+    }
+
+    /**
+     * Return object 10
+     *
+     * @access public
+     * @return string $object10
+     */
+    public function getObject10(): string
+    {
+        return $this->object10;
+    }
+
+    /**
+     * Set object 11
+     *
+     * @access public
+     * @param string $object11
+     */
+    public function setObject11($object11): void
+    {
+        $this->object11 = $object11;
+    }
+
+    /**
+     * Return object 11
+     *
+     * @access public
+     * @return string $object11
+     */
+    public function getObject11(): string
+    {
+        return $this->object11;
+    }
+
+    /**
+     * Set object 12
+     *
+     * @access public
+     * @param string $object12
+     */
+    public function setObject12($object12): void
+    {
+        $this->object12 = $object12;
+    }
+
+    /**
+     * Return object 12
+     *
+     * @access public
+     * @return string $object12
+     */
+    public function getObject12(): string
+    {
+        return $this->object12;
     }
 }
