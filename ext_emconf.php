d58522f68c658ddcdbe31c0cb8c9c0cc4ee20420
Michael Schams <typo3@2020.schams.net>
2020-10-08 12:01:15 +1100

    [TASK] Clean up composer.json and ext_emconf.php files

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,6 +27,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Michael Schams (schams.net)',
     'author_email' => 'schams.net',
     'author_company' => 'https://schams.net',
+    'autoload' => [
+        'psr-4' => [
+            'SchamsNet\\AwsImageRecognition\\' => 'Classes'
+        ]
+    ],
     'constraints' => [
         'depends' => [
             'typo3' => '10.0.0-10.4.999',
